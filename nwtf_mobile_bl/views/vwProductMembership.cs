921ecdf7219080ee075897071b8230650b3f695b
Rona Melissa Plana <rona@lanesystems.com>
2020-03-27 12:37:54 +0800

    renamed productID to productUID since it is GUID

--- nwtf_mobile_bl/views/vwProductMembership.cs
+++ nwtf_mobile_bl/views/vwProductMembership.cs
@@ -8,7 +8,7 @@ namespace nwtf_mobile_bl.views
     class vwProductMembership
     {
         [PrimaryKey, NotNull] static Guid id { get; set; }
-        [NotNull] Guid productID { get; set; }
+        [NotNull] Guid productUID { get; set; }
         [NotNull] int fromDays { get; set; }
         [NotNull] int toDays { get; set; }
         [NotNull] decimal amount { get; set; }
