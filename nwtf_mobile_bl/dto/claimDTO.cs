d60a6c34a4151aaa8445a01f3676fede69873614
Dwight Kenn Caballero <kenn@lanesystems.com>
2020-03-27 14:20:20 +0800

    added claimDTO

---
+++ nwtf_mobile_bl/dto/claimDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwtf_mobile_bl.dto
+{
+    public class claimDTO
+    {
+
+        views.vwClaimTransaction claimTransaction { get; set; }
+
+        List<views.vwCustomer> listCustomer { get; set; }
+        List<views.vwMafEnrollmentClosure> listMAF { get; set; }
+        List<views.vwClaimant> listClaimants { get; set; }
+        List<views.vwClaimTypes> listClaimTypes { get; set; }
+
+        claimDTO()
+        {
+            claimTransaction = new views.vwClaimTransaction();
+
+            listCustomer = new List<views.vwCustomer>();
+            listMAF = new List<views.vwMafEnrollmentClosure>();
+            listClaimants = new List<views.vwClaimant>();
+            listClaimTypes = new List<views.vwClaimTypes>();
+        }
+    }
+}
---
+++ nwtf_mobile_bl/views/vwClaimant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwtf_mobile_bl.views
+{
+    public class vwClaimant
+    {
+        Guid id { get; set; }
+        Guid customerID { get; set; }
+        string claimantID { get; set; }
+        string claimantFullName { get; set; }
+        DateTime claiamantBirthdate { get; set; }
+        string claimantType { get; set; }
+        string claimantRelation { get; set; }
+    }
+}
