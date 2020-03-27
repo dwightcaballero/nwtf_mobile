b348463604a4a80a0332179bef867391fce024b7
Dwight Kenn Caballero <kenn@lanesystems.com>
2020-03-27 14:58:12 +0800

    updated claimDTO

--- nwtf_mobile_bl/dto/claimDTO.cs
+++ nwtf_mobile_bl/dto/claimDTO.cs
@@ -7,21 +7,34 @@ namespace nwtf_mobile_bl.dto
     public class claimDTO
     {
 
-        views.vwClaimTransaction claimTransaction { get; set; }
+        Guid id { get; set; }
+        string claimTransactionReferenceNumber { get; set; }
+
+        views.vwCustomer customer { get; set; }
+        views.vwMafEnrollmentClosure maf { get; set; }
+
+        string claimantID { get; set; }
+        int claimantType { get; set; }
+        string defaultPayeeID { get; set; }
+        string defaultPayeeName { get; set; }
+        int defaultPayeeType { get; set; }
+
+        Guid blockID { get; set; }
+        Guid branchID { get; set; }
 
         List<views.vwCustomer> listCustomer { get; set; }
         List<views.vwMafEnrollmentClosure> listMAF { get; set; }
         List<views.vwClaimant> listClaimants { get; set; }
         List<views.vwClaimTypes> listClaimTypes { get; set; }
+        List<views.vwClaimTypes> listSelectedClaimTypes { get; set; }
 
         claimDTO()
         {
-            claimTransaction = new views.vwClaimTransaction();
-
             listCustomer = new List<views.vwCustomer>();
             listMAF = new List<views.vwMafEnrollmentClosure>();
             listClaimants = new List<views.vwClaimant>();
             listClaimTypes = new List<views.vwClaimTypes>();
+            listSelectedClaimTypes = new List<views.vwClaimTypes>();
         }
     }
 }
---
+++ nwtf_mobile_bl/views/vwClaimTypeRequiredDocumentImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwtf_mobile_bl.views
+{
+    public class vwClaimTypeRequiredDocumentImage
+    {
+        Guid id { get; set; }
+        Guid claimTypeRequiredDocumentID { get; set; }
+        string requiredDocumentImageName { get; set; }
+        string requiredDocumentDescription { get; set; }
+        byte requiredDocumentFile { get; set; }
+    }
+}
