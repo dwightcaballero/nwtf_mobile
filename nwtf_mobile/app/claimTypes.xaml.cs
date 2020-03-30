9bf2865397c089bbf867018f5823de753214e894
Rona Melissa Plana <rona@lanesystems.com>
2020-03-30 18:50:45 +0800

    updated create claims

--- nwtf_mobile/app/claimTypes.xaml.cs
+++ nwtf_mobile/app/claimTypes.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.CustomControls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using static nwtf_mobile_bl.views;
@@ -13,7 +14,66 @@ namespace nwtf_mobile.app
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class claimTypes : ContentView
     {
-        public vwClaimTypes claimtypesample = new vwClaimTypes();
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
+            nameof(RepeaterView.ItemTemplate),
+            typeof(DataTemplate),
+            typeof(RepeaterView),
+            default(DataTemplate));
+
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            nameof(RepeaterView.ItemTemplate),
+            typeof(ICollection),
+            typeof(RepeaterView),
+            null,
+            BindingMode.OneWay,
+            propertyChanged: ItemsChanged);
+
+        public ICollection ItemsSource
+        {
+            get => (ICollection)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+
+        protected virtual View ViewFor(object item)
+        {
+            View view = null;
+
+            if (ItemTemplate != null)
+            {
+                var content = ItemTemplate.CreateContent();
+
+                view = content is View ? content as View : ((ViewCell)content).View;
+
+                view.BindingContext = item;
+            }
+
+            return view;
+        }
+
+        private static void ItemsChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = bindable as RepeaterView;
+
+            if (control == null) return;
+
+            control.Children.Clear();
+
+            var items = (ICollection)newValue;
+
+            if (items == null) return;
+
+            foreach (var item in items)
+            {
+            }
+        }
+
+    public vwClaimTypes claimtypesample = new vwClaimTypes();
         public List<vwClaimTypes> claimTypeList = new List<vwClaimTypes>();
         public claimTypes()
         {
@@ -21,16 +81,32 @@ namespace nwtf_mobile.app
             // Only Sample Data
             claimtypesample.claimBenefit = "Number of Days";
             claimtypesample.forAdvance = true;
-            claimtypesample.claimTypeName = "Claim Type A";                       
+            claimtypesample.claimTypeName = "Claim Type A";
             claimTypeList.Add(claimtypesample);
             claimtypesample = new vwClaimTypes();
             claimtypesample.claimBenefit = "Premiums Paid";
-            claimtypesample.forAdvance = true;
+            claimtypesample.forAdvance = false;
             claimtypesample.claimTypeName = "Claim Type B";
             claimTypeList.Add(claimtypesample);
             claimTypeRepeater.ItemsSource = claimTypeList;
-           
-        }
+            var control = claimTypeRepeater as RepeaterView;
 
-         }
+            if (control == null) return;
+            foreach (vwClaimTypes item in control.ItemsSource)
+            {
+                if (item.forAdvance == true)
+                {
+                    foreach (View item1 in control.Children)
+                    {
+                        
+                        Label change = item1.FindByName<Label>("forAdvancePanel");
+                        if (change == null) return;
+                        //  set for advance panel visible to false
+                    }
+                }
+            }
+        }
+       
+
+    }
 }
