09e75f2fc70ab76dc69f372fcb8ed0e582b232b6
Dwight Kenn Caballero <kenn@lanesystems.com>
2020-03-30 10:10:02 +0800

    update on the controller

--- nwtf_mobile_bl/controllers/claimTransaction.cs
+++ nwtf_mobile_bl/controllers/claimTransaction.cs
@@ -6,10 +6,5 @@ namespace nwtf_mobile_bl.controllers
 {
     public class claimTransaction
     {
-        public List<views.vwCustomer> getListCustomers()
-        {
-            dataservices.claimTransaction ct = new dataservices.claimTransaction();
-            return ct.getListCustomers();
-        }
-    }
+    }      
 }
