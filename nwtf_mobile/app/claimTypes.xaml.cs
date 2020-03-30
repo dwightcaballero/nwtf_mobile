2ce1b4231f2510043913be9ec6b4f24407f19ed6
Rona Melissa Plana <rona@lanesystems.com>
2020-03-30 13:54:47 +0800

    add repeater view in claim types (sample data only)

---
+++ nwtf_mobile/app/claimTypes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using nwtf_mobile_bl.views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace nwtf_mobile.app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class claimTypes : ContentView
+    {
+        public vwClaimTypes claimtypesample = new vwClaimTypes();
+        public List<vwClaimTypes> claimTypeList = new List<vwClaimTypes>();
+        public claimTypes()
+        {
+            InitializeComponent();
+            // Only Sample Data
+            claimtypesample.claimBenefit = "Number of Days";
+            claimtypesample.forAdvance = true;
+            claimtypesample.claimTypeName = "Claim Type A";                       
+            claimTypeList.Add(claimtypesample);
+            claimTypeRepeater.ItemsSource = claimTypeList;
+           
+        }
+
+         }
+}
