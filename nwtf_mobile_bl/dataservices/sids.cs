cb5f0b806b961036df4dac08bca45e927731a9f4
Dwight Kenn Caballero <kenn@lanesystems.com>
2020-03-31 17:26:10 +0800

    updated bl and packages

--- nwtf_mobile_bl/dataservices/sids.cs
+++ nwtf_mobile_bl/dataservices/sids.cs
@@ -4,25 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace nwtf_mobile_bl.dataservices
+namespace nwtf_mobile_bl
 {
-    public static class sids
-    {
-        public static views.vwSids getSID(Guid uid)
+    public partial class dataservices{
+        public static class sids
         {
-            views.vwSids pRec = new views.vwSids();
-            using (SQLiteConnection conn = new SQLiteConnection(nwtf_mobile_bl.dataservices.Database.DatabasePath))
+            public static views.vwSids getSID(Guid uid)
             {
-                string sql = "SELECT id, sid, authobj, authref FROM vwSids WHERE sid=?";
-                pRec = conn.Query<views.vwSids>(sql, uid).FirstOrDefault();
+                views.vwSids pRec = new views.vwSids();
+                using (SQLiteConnection conn = new SQLiteConnection(Database.DatabasePath))
+                {
+                    string sql = "SELECT id, sid, authobj, authref FROM vwSids WHERE sid=?";
+                    pRec = conn.Query<views.vwSids>(sql, uid).FirstOrDefault();
 
-                if (pRec == null)
-                {
-                    return pRec;
-                }
-                else
-                {
-                    return pRec;
+                    if (pRec == null)
+                    {
+                        return pRec;
+                    }
+                    else
+                    {
+                        return pRec;
+                    }
                 }
             }
         }
