5418cb161e503425a5827fd0c529df947393c041
Rona Melissa Plana <rona@lanesystems.com>
2020-03-31 10:03:40 +0800

    hide for advance processing if allowAdvances value is false

--- nwtf_mobile/app/claimTypes.xaml.cs
+++ nwtf_mobile/app/claimTypes.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,99 +15,57 @@ namespace nwtf_mobile.app
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class claimTypes : ContentView
     {
-        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
-            nameof(RepeaterView.ItemTemplate),
-            typeof(DataTemplate),
-            typeof(RepeaterView),
-            default(DataTemplate));
-
-        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
-            nameof(RepeaterView.ItemTemplate),
-            typeof(ICollection),
-            typeof(RepeaterView),
-            null,
-            BindingMode.OneWay,
-            propertyChanged: ItemsChanged);
-
-        public ICollection ItemsSource
-        {
-            get => (ICollection)GetValue(ItemsSourceProperty);
-            set => SetValue(ItemsSourceProperty, value);
-        }
-
-        public DataTemplate ItemTemplate
-        {
-            get => (DataTemplate)GetValue(ItemTemplateProperty);
-            set => SetValue(ItemTemplateProperty, value);
-        }
-
-        protected virtual View ViewFor(object item)
-        {
-            View view = null;
-
-            if (ItemTemplate != null)
-            {
-                var content = ItemTemplate.CreateContent();
-
-                view = content is View ? content as View : ((ViewCell)content).View;
-
-                view.BindingContext = item;
-            }
-
-            return view;
-        }
-
-        private static void ItemsChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            var control = bindable as RepeaterView;
-
-            if (control == null) return;
-
-            control.Children.Clear();
-
-            var items = (ICollection)newValue;
-
-            if (items == null) return;
-
-            foreach (var item in items)
-            {
-            }
-        }
-
-    public vwClaimTypes claimtypesample = new vwClaimTypes();
+      
+        public vwClaimTypes claimtypesample = new vwClaimTypes();
         public List<vwClaimTypes> claimTypeList = new List<vwClaimTypes>();
-        public claimTypes()
+
+        public void PopulateClaimTypes()
         {
-            InitializeComponent();
             // Only Sample Data
             claimtypesample.claimBenefit = "Number of Days";
-            claimtypesample.forAdvance = true;
+            claimtypesample.allowAdvances = true;
             claimtypesample.claimTypeName = "Claim Type A";
             claimTypeList.Add(claimtypesample);
             claimtypesample = new vwClaimTypes();
             claimtypesample.claimBenefit = "Premiums Paid";
-            claimtypesample.forAdvance = false;
+            claimtypesample.allowAdvances = false;
             claimtypesample.claimTypeName = "Claim Type B";
             claimTypeList.Add(claimtypesample);
             claimTypeRepeater.ItemsSource = claimTypeList;
+        }
+
+        public void AccessControlsInRepeater()
+        {
             var control = claimTypeRepeater as RepeaterView;
 
             if (control == null) return;
-            foreach (vwClaimTypes item in control.ItemsSource)
+            foreach (Grid item1 in control.Children)
             {
-                if (item.forAdvance == true)
+                Label forAdvancePanel = (Label)item1.Children[4];
+                Label checkForAdvance = (Label)item1.Children[5];
+
+                foreach (vwClaimTypes item in control.ItemsSource)
                 {
-                    foreach (View item1 in control.Children)
+                    if (item.forAdvance == false)
                     {
-                        
-                        Label change = item1.FindByName<Label>("forAdvancePanel");
-                        if (change == null) return;
-                        //  set for advance panel visible to false
-                    }
+                        if (checkForAdvance == null) return;
+                        if (item.forAdvance.ToString() == checkForAdvance.Text)
+                        {
+                            if (forAdvancePanel == null) return;
+                            //  set for advance panel visible to false
+                            forAdvancePanel.IsVisible = false;
+                        }
+                        continue;
+                    }                  
                 }
             }
         }
-       
-
+        public claimTypes()
+        {
+            InitializeComponent();
+            PopulateClaimTypes();
+            AccessControlsInRepeater();
+          
+        }             
     }
 }
