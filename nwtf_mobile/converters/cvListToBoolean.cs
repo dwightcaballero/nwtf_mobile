f4887a38060c8cca0d330ab983379a07ad1e7530
Dwight Kenn Caballero <kenn@lanesystems.com>
2020-04-03 14:59:36 +0800

    added converters

---
+++ nwtf_mobile/converters/cvListToBoolean.cs
@@ -0,0 +1,29 @@
+﻿using nwtf_mobile_bl;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace nwtf_mobile.converters
+{
+    class cvListToBoolean : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return ((List<views.vwClaimant>)value).Count > 1;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
