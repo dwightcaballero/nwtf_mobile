1b04c1c4fb957ec0da96a161148f1d20cd9eea1b
Sweet Angel Altamera <sweet@DEVW10-05>
2020-04-08 17:29:25 +0800

    required documents update

--- nwtf_mobile/app/requiredDocuments.xaml.cs
+++ nwtf_mobile/app/requiredDocuments.xaml.cs
@@ -9,6 +9,12 @@ using Plugin.Media;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Media.Abstractions;
+using Android.Widget;
+using Android.Content;
+using Android.Provider;
+using Android.Runtime;
+using Android.App;
 
 namespace nwtf_mobile.app
 {
@@ -24,6 +30,7 @@ namespace nwtf_mobile.app
             pcon.getRequiredDocuments();
             stkAddFile.IsVisible = false;
             stkUploadRequiredDocument.IsVisible = false;
+            stkRequiredDocuments.IsVisible = false;
         }
 
         private void Pcon_loadrequiredDocuments(object sender, List<views.vwRequiredDocuments> e)
@@ -35,12 +42,41 @@ namespace nwtf_mobile.app
         {
             await CrossMedia.Current.Initialize();
 
-            //var file 
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                CompressionQuality = 40,
+                Name = "try.jpg",
+                Directory = "sample"
+            }) ;
+
+            if(file == null)
+            {
+                return;
+            }
+
+        }
+
+        async void UploadPhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if(!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                //Toast.MakeText(this, "upload not supported on this device", ToastLength.Short).Show();
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Full,
+                CompressionQuality=40
+            }) ;
         }
 
         private void btnFileUpload_Clicked(object sender, EventArgs e)
         {
-            Button btnFileUpload = (Button)sender;
+            Xamarin.Forms.Button btnFileUpload = (Xamarin.Forms.Button)sender;
             var row = Grid.GetRow(btnFileUpload);
             var grd = btnFileUpload.Parent as Grid;
             var reqDocName = grd.Children.Where(child => Grid.GetRow(child) == row && Grid.GetColumn(child) == 0).FirstOrDefault();
@@ -53,12 +89,36 @@ namespace nwtf_mobile.app
 
             stkRequiredDocuments.IsVisible = false;
             stkUploadRequiredDocument.IsVisible = true;
+            lstVwUploadedFiles.IsVisible = false;
         }
+
+        //protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode)
+        //{
+        //    base.OnActivityResult(requestCode, resultCode, data);
+        //}
 
         private void btnCapture_Clicked(object sender, EventArgs e)
         {
-
+            //Intent intent = new Intent(MediaStore.ActionImageCapture);
+            //StartActivityForResult(intent, 0);
+            stkAddFile.IsVisible = true;
+            TakePhoto();
         }
 
+        private void btnUpload_Clicked(object sender, EventArgs e)
+        {
+            stkAddFile.IsVisible = true;
+            UploadPhoto();
+        }
+
+        private void btnOK_Clicked(object sender, EventArgs e)
+        {
+            stkAddFile.IsVisible = false;
+        }
+
+        private void btnCancel_Clicked(object sender, EventArgs e)
+        {
+            stkAddFile.IsVisible = false;
+        }
     }
 }
